c8f5739e6c31993577bacf936dd9829f7ef89e0c
TorbenSchucht <torben.schucht@uni-oldenburg.de>
2024-11-14 17:59:19 +0100

    Added Data of Dispersal and 500 Species Simulation

--- AdditionalFigures/FigureGeneration.m
+++ AdditionalFigures/FigureGeneration.m
@@ -71,3 +71,115 @@ for t = TimePoints
     xlabel('x')
     text(0.65,max(max(N))*0.9,sprintf('t = %d',t))
 end
+
+%%
+%% Survival matrix (500 Species simulation)
+load('500SpeciesFull.mat')
+
+subplot(2,5,4:5)
+ThetaValues = NSurvivalTime.(3);
+SurvivedSpecies = NSurvivalTime.SpeciesID(NSurvivalTime.Survival_Time == 1000000);
+NxPad = N(:,0.25*p.L+1:0.75*p.L); % N without outside pad
+[~,Nmax] = max(N,[],2);
+scatter(xProj(Nmax),ThetaValues(SurvivedSpecies),20,'filled')
+xlabel('x_{max}')
+ylabel('\theta')
+text([0.05, 0.25, 0.4, 0.5, 0.65, 0.85], [3, 2.1, 1.5, 1.1, 0.8, 0.3], ["1", "13", "23", "29", "19","6"], "FontSize", 10);
+xlim([0, 1])
+ylim([0, 3])
+
+subplot(2,5,1:3)
+TotalSp = length(Rsort);
+for i = 1:TotalSp
+   ExtPoint = NSurvivalTime.Survival_Time(i);
+   plot([0,log10(ExtPoint)], [NSurvivalTime.(3)(i), NSurvivalTime.(3)(i)], 'blue')
+   hold on
+end
+hold off
+xlabel('log(t)')
+ylabel('\theta')
+xlim([3,6])
+
+subplot(2,5,9:10)
+xProj = linspace(0,1,p.L);
+plotCommunity(xProj,N,t)
+xlabel('x')
+hold on
+[~, D] = ShannonIndex(N, 4);
+yyaxis right
+plot(xProj(2:8:end),D,'LineWidth', 2)
+ylabel('D_{l}')
+str = sprintf('t = 10^{%d}',log10(t));
+xL=xlim;
+yL=ylim;
+text(0.99*xL(2),0.99*yL(2),str,'HorizontalAlignment','right','VerticalAlignment','top')
+
+
+subplot(2,5,6:8)
+ExtEvents = NSurvivalTime.(2);
+ExtEvents = ExtEvents(NSurvivalTime.(2)<1000000);
+histogram(log10(ExtEvents),100)
+xlim([3, 6])
+ylabel('Extinctions')
+xlabel('log(t)')
+% Add Global diversity
+hold on
+yyaxis right
+plot(log10(HPoints),HGlobal,'LineWidth',2)
+ylabel('D_{Global}')
+
+
+%% Community and temporal diversity patterns for different dispersal widths
+
+load('IncreasingDispersalSingleSimulations')
+
+% Communities for different sigma
+CommIdx = [1, 30, 100];
+SubplotIdx = [5,3,1];
+for i = 1:length(CommIdx)
+    subplot(3,2,SubplotIdx(i))
+    plotCommunity(xProj,NendMatrix(:,:,CommIdx(i)),t)
+    ttlString = sprintf('\\sigma = %.g \n t = 10^{%d}', sigmaIncrease(CommIdx(i)),log10(t));
+    if i == 1
+       ttlString = sprintf('\\sigma = %.g \n t = 10^{%d}', sigmaIncrease(CommIdx(i)),log10(t));
+    end
+    xL=xlim;
+    yL=ylim;
+    text(0.99*xL(2),0.99*yL(2),ttlString,'HorizontalAlignment','right','VerticalAlignment','top')
+
+    if i == 1
+       xlabel('x') 
+    end
+    N = NendMatrix(:,:,CommIdx(i));
+    biomass = sum(N,1);
+    hold on
+    plot(linspace(0,1,round(L/2)), biomass(round(L*0.25)+1:round(L*0.75)), 'black--')
+    hold off
+    ylabel('N_{i}')
+end
+
+% Colored diversity for different sigmas
+subplot(3,2,[2,4,6])
+cutoff = 100; % Stop point
+HLocalEnd = squeeze(HLocalMatrix(:,99,1:cutoff));
+y = sigmaIncrease(1:cutoff);
+pcolor(xP,y,HLocalEnd')
+shading interp
+a = colorbar();
+ylabel('\sigma');
+xlabel('x');
+colormap('jet')
+ylabel(a, 'D_{l}', 'FontSize', 9)
+
+%%
+% Function to plot Community composition
+function plotCommunity(x,N,t)
+    for i = 1:size(N,1)
+        plot(x(1:2:end),N(i,size(N,2)*0.25:(size(N,2)*0.75)-1),'Linewidth', 0.8)
+        hold on
+    end
+hold off
+ylabel('N_{i}')
+xlabel('x')
+end
+
